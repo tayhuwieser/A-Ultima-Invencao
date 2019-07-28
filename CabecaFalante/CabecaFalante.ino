9cfd080dddc2ff5ba7e11aaaaf6097f22f2df84d
Tayhú Wieser <tayhu.wieser@icloud.com>
2019-07-28 12:53:16 -0300

    upd CabecaFalante

--- CabecaFalante/CabecaFalante.ino
+++ CabecaFalante/CabecaFalante.ino
@@ -28,9 +28,17 @@ void setupReceiver1() {
 }
 
 int readReceiver1() {
-    //lê o sinal do controle (de 1020 à 1880) e converte p/ um valor de 20 à 160
+    //lê o sinal do controle (de 1020 à 1880) e converte p/ um valor de 120 à 150
     int analogValue = pulseIn(RC_RECEIVER_PIN1, HIGH, 50000);
-    return map(analogValue, 1020, 1880, 20, 160);
+    Serial.println(analogValue);
+    if (analogValue = 0)
+    {
+      pwm.setPWM(1, 0, 1500);
+    }
+    else
+    {
+      return map(analogValue, 1020, 1880, 120, 150 );
+    } 
 }
 
 void servo1Loop(int value1) {
@@ -41,9 +49,9 @@ void servo1Loop(int value1) {
 }
 
 int pulseWidth(int angle) {
-  // Recebe valor de 20 à 160 e transforma no valor pra uso no pwm
+  // Recebe valor de x à y e transforma no valor pra uso no pwm
   int pulse_wide, analog_value;
-  pulse_wide   = map(angle, 20, 160, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+  pulse_wide   = map(angle, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
   analog_value = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096);
   return analog_value;
 } 
